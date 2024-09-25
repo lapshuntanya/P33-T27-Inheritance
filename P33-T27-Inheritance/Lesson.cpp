490c19d39cefda2a2c9d06b43361bb5b7cacb969
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-25 17:27:34 +0300

    definition class Lesson

--- P33-T27-Inheritance/Lesson.cpp
+++ P33-T27-Inheritance/Lesson.cpp
@@ -1,44 +1,52 @@
 #include "Lesson.h"
 
-Lesson::Lesson()
-{
+Lesson::Lesson(){
+    theme = "C++";
+    info = "Inheritance";
+    home = "Create a base class!";
+    cout << "Lesson()\n";
 }
 
-Lesson::Lesson(string theme, string info, string home)
-{
+Lesson::Lesson(string theme, string info, string home){
+    this->theme = theme;
+    this->info = info;
+    this->home = home;
+    cout << "Lesson(t, i, h)\n";
 }
 
-Lesson::~Lesson()
-{
+Lesson::~Lesson(){
+    cout << "~Lesson() => " << theme << endl;
 }
 
-void Lesson::setTheme(string theme)
-{
+
+void Lesson::setTheme(string theme){
+    this->theme = theme;
 }
 
-void Lesson::setInfo(string info)
-{
+void Lesson::setInfo(string info){
+    this->info = info;
 }
 
-void Lesson::setHome(string home)
-{
+void Lesson::setHome(string home){
+    this->home = home;
 }
 
-string Lesson::getTheme() const
-{
-    return string();
+string Lesson::getTheme() const{
+    return theme;
 }
 
-string Lesson::getInfo() const
-{
-    return string();
+string Lesson::getInfo() const{
+    return info;
 }
 
-string Lesson::getHome() const
-{
-    return string();
+string Lesson::getHome() const{
+    return home;
 }
 
 void Lesson::showInfo() const
 {
+    cout << "\n\n================================\n";
+    cout << "Theme: " << theme << endl;
+    cout << "Info:  " << info << endl;
+    cout << "Home:  " << home << endl;
 }
