448052992e55a38968f3b2487e76c83175257a70
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-25 17:19:33 +0300

    class Lesson

---
+++ P33-T27-Inheritance/Lesson.cpp
@@ -0,0 +1,44 @@
+#include "Lesson.h"
+
+Lesson::Lesson()
+{
+}
+
+Lesson::Lesson(string theme, string info, string home)
+{
+}
+
+Lesson::~Lesson()
+{
+}
+
+void Lesson::setTheme(string theme)
+{
+}
+
+void Lesson::setInfo(string info)
+{
+}
+
+void Lesson::setHome(string home)
+{
+}
+
+string Lesson::getTheme() const
+{
+    return string();
+}
+
+string Lesson::getInfo() const
+{
+    return string();
+}
+
+string Lesson::getHome() const
+{
+    return string();
+}
+
+void Lesson::showInfo() const
+{
+}
