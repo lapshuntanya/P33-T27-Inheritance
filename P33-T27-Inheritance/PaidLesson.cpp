ad3f921cd025372b4e8b084c4a31801d6cb2fbd7
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-25 18:07:33 +0300

    class PaidLesson

--- P33-T27-Inheritance/PaidLesson.cpp
+++ P33-T27-Inheritance/PaidLesson.cpp
@@ -1,1 +1,37 @@
 #include "PaidLesson.h"
+
+PaidLesson::PaidLesson(){
+     //Lesson();
+    price = 0;
+    cout << "PaidLesson()\n";
+}
+
+PaidLesson::PaidLesson(string theme, string info, string home, float price)
+    :Lesson(theme, info, home)
+{
+    //Lesson();
+   /* this->theme = theme;
+    this->info = info;
+    setHome(home);*/
+
+    this->price = price;
+    cout << "PaidLesson(t, i, h, p)\n";
+}
+
+PaidLesson::~PaidLesson(){
+    cout << "~PaidLesson() => " << theme << endl;
+}
+
+void PaidLesson::setPrice(float price){
+    this->price = price;
+}
+
+float PaidLesson::getPrice() const
+{
+    return price;
+}
+
+void PaidLesson::showInfo() const{
+    Lesson::showInfo(); // theme, info, home
+    cout << "Price: " << price << "UAH\n\n";
+}
--- P33-T27-Inheritance/PaidLesson.h
+++ P33-T27-Inheritance/PaidLesson.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Lesson.h"
+
 class PaidLesson : public Lesson
 {
 protected:
